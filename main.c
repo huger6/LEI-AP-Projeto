a9541dabf0710a8dc28ff37968bd2a6b54658450
Hugo <160261553+hugor@users.noreply.github.com>
2024-11-23 21:26:40 +0000

    Estrutura

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <locale.h>
+
+//TODO LIST
+/*
+
+
+
+*/
+
+
+
+//Structs
+
+
+
+//Protótipos das funções
+
+
+
+
+//Main
+
+
+
+
+
+
+
+
+//Funções
+
+
